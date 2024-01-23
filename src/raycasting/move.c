7d751a3b2ea1e186dbf489d6301d5d7adc8f8ed5
Dominique Maessen <dmaessen@f0r1s12.codam.nl>
2024-01-23 17:08:08 +0100

    changes made

--- src/raycasting/move.c
+++ src/raycasting/move.c
@@ -6,7 +6,7 @@
 /*   By: dmaessen <dmaessen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 13:45:23 by dmaessen          #+#    #+#             */
-/*   Updated: 2024/01/18 16:16:57 by dmaessen         ###   ########.fr       */
+/*   Updated: 2024/01/23 16:45:41 by dmaessen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void move_up(t_data *data) // checking if walls
 	double moveSpeed;
 
 	moveSpeed = 0.55; // check on these values - or 0.8
-	if (data->input->parsed_map[(int)(data->m->posX + data->m->dirX * moveSpeed)][(int)data->m->posY] == '0')
-		data->m->posX += data->m->dirX * moveSpeed;
-	if (data->input->parsed_map[(int)data->m->posX][(int)(data->m->posY + data->m->dirY * moveSpeed)] == '0')
-		data->m->posY += data->m->dirY * moveSpeed; 
+	if (data->input->parsed_map[(int)(data->m->pos_x + data->m->dir_x * moveSpeed)][(int)data->m->pos_y] == '0')
+		data->m->pos_x += data->m->dir_x * moveSpeed;
+	if (data->input->parsed_map[(int)data->m->pos_x][(int)(data->m->pos_y + data->m->dir_y * moveSpeed)] == '0')
+		data->m->pos_y += data->m->dir_y * moveSpeed; 
 }
 
 void move_down(t_data *data)
@@ -34,23 +34,23 @@ void move_down(t_data *data)
 	double moveSpeed;
 
 	moveSpeed = 0.55;
-	if (data->input->parsed_map[(int)(data->m->posX - data->m->dirX * moveSpeed)][(int)data->m->posY] == '0')
-		data->m->posX -= data->m->dirX * moveSpeed;
-	if (data->input->parsed_map[(int)data->m->posX][(int)(data->m->posY - data->m->dirY * moveSpeed)] == '0')
-		data->m->posY -= data->m->dirY * moveSpeed;
+	if (data->input->parsed_map[(int)(data->m->pos_x - data->m->dir_x * moveSpeed)][(int)data->m->pos_y] == '0')
+		data->m->pos_x -= data->m->dir_x * moveSpeed;
+	if (data->input->parsed_map[(int)data->m->pos_x][(int)(data->m->pos_y - data->m->dir_y * moveSpeed)] == '0')
+		data->m->pos_y -= data->m->dir_y * moveSpeed;
 }
 
 // These have to be made the same way as the up and down keys, 
-// but use planeX and planeY instead of dirX and dirY.
+// but use plane_x and plane_y instead of dir_x and dir_y.
 void move_left(t_data *data)
 {
 	double moveSpeed;
 
 	moveSpeed = 0.55;
-	if (data->input->parsed_map[(int)(data->m->posX + data->m->planeX * moveSpeed)][(int)data->m->posY] == '0')
-		data->m->posX += data->m->planeX * moveSpeed;
-	if (data->input->parsed_map[(int)data->m->posX][(int)(data->m->posY + data->m->planeY * moveSpeed)] == '0')
-		data->m->posY += data->m->planeY * moveSpeed;
+	if (data->input->parsed_map[(int)(data->m->pos_x + data->m->plane_x * moveSpeed)][(int)data->m->pos_y] == '0')
+		data->m->pos_x += data->m->plane_x * moveSpeed;
+	if (data->input->parsed_map[(int)data->m->pos_x][(int)(data->m->pos_y + data->m->plane_y * moveSpeed)] == '0')
+		data->m->pos_y += data->m->plane_y * moveSpeed;
 }
 
 void move_right(t_data *data)
@@ -58,40 +58,40 @@ void move_right(t_data *data)
 	double moveSpeed;
 
 	moveSpeed = 0.55;
-	if (data->input->parsed_map[(int)(data->m->posX - data->m->planeX * moveSpeed)][(int)data->m->posY] == '0')
-		data->m->posX -= data->m->planeX * moveSpeed;
-	if (data->input->parsed_map[(int)data->m->posX][(int)(data->m->posY - data->m->planeY * moveSpeed)] == '0')
-		data->m->posY -= data->m->planeY * moveSpeed;
+	if (data->input->parsed_map[(int)(data->m->pos_x - data->m->plane_x * moveSpeed)][(int)data->m->pos_y] == '0')
+		data->m->pos_x -= data->m->plane_x * moveSpeed;
+	if (data->input->parsed_map[(int)data->m->pos_x][(int)(data->m->pos_y - data->m->plane_y * moveSpeed)] == '0')
+		data->m->pos_y -= data->m->plane_y * moveSpeed;
 }
 
 void look_left(t_data *data) // camera direction and camera plane must be rotated
 {
 	double rotSpeed;
-	double oldDirX;
-	double oldPlaneX;
+	double olddir_x;
+	double oldplane_x;
 
 	rotSpeed = 0.1; // check on these values -- or 0.3
-	oldDirX = data->m->dirX;
-	data->m->dirX = data->m->dirX * cos(rotSpeed) - data->m->dirY * sin(rotSpeed);
-	data->m->dirY = oldDirX * sin(rotSpeed) + data->m->dirY * cos(rotSpeed);
-	oldPlaneX = data->m->planeX;
-	data->m->planeX = data->m->planeX * cos(rotSpeed) - data->m->planeY * sin(rotSpeed);
-	data->m->planeY = oldPlaneX * sin(rotSpeed) + data->m->planeY * cos(rotSpeed);
+	olddir_x = data->m->dir_x;
+	data->m->dir_x = data->m->dir_x * cos(rotSpeed) - data->m->dir_y * sin(rotSpeed);
+	data->m->dir_y = olddir_x * sin(rotSpeed) + data->m->dir_y * cos(rotSpeed);
+	oldplane_x = data->m->plane_x;
+	data->m->plane_x = data->m->plane_x * cos(rotSpeed) - data->m->plane_y * sin(rotSpeed);
+	data->m->plane_y = oldplane_x * sin(rotSpeed) + data->m->plane_y * cos(rotSpeed);
 }
 
 void look_right(t_data *data)
 {
 	double rotSpeed;
-	double oldDirX;
-	double oldPlaneX;
+	double olddir_x;
+	double oldplane_x;
 
 	rotSpeed = 0.1;
-	oldDirX = data->m->dirX;
-	oldPlaneX = data->m->planeX;
-	data->m->dirX = data->m->dirX * cos(-rotSpeed) - data->m->dirY * sin(-rotSpeed);
-	data->m->dirY = oldDirX * sin(-rotSpeed) + data->m->dirY * cos(-rotSpeed);
-	data->m->planeX = data->m->planeX * cos(-rotSpeed) - data->m->planeY * sin(-rotSpeed);
-	data->m->planeY = oldPlaneX * sin(-rotSpeed) + data->m->planeY * cos (-rotSpeed);
+	olddir_x = data->m->dir_x;
+	oldplane_x = data->m->plane_x;
+	data->m->dir_x = data->m->dir_x * cos(-rotSpeed) - data->m->dir_y * sin(-rotSpeed);
+	data->m->dir_y = olddir_x * sin(-rotSpeed) + data->m->dir_y * cos(-rotSpeed);
+	data->m->plane_x = data->m->plane_x * cos(-rotSpeed) - data->m->plane_y * sin(-rotSpeed);
+	data->m->plane_y = oldplane_x * sin(-rotSpeed) + data->m->plane_y * cos (-rotSpeed);
 }
 
 void move(void *param)
