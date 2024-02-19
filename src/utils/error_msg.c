07467e1f9133c22f67731369ae702d48d831b167
Dominique Maessen <dmaessen@f0r2s14.codam.nl>
2024-02-19 13:49:19 +0100

    changes

--- src/utils/error_msg.c
+++ src/utils/error_msg.c
@@ -6,7 +6,7 @@
 /*   By: dmaessen <dmaessen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 10:41:30 by dmaessen          #+#    #+#             */
-/*   Updated: 2023/10/31 10:41:31 by dmaessen         ###   ########.fr       */
+/*   Updated: 2024/02/19 13:46:54 by dmaessen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,18 @@
 #include "../include/utils.h"
 #include <unistd.h>
 
-void	err_msg(char *str)
+static int free_from_pos(int i)
+{
+	if (i = 1)
+		// something
+	if (i = 2)
+		// something else
+}
+
+void	err_msg(char *str, int i)
 {
 	write(2, "Error\n", 6);
 	write(2, str, ft_strlen(str));
+	free_from_pos(i);
 	exit (1);
 }
